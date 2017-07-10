cde182574f4b40ae34d48458aeeeafe4087e683a
Kayle Sillito <sil14003@byui.edu>
2017-07-10 17:12:19 -0600

    body parser

--- index.js
+++ index.js
@@ -15,6 +15,13 @@ const pool = new Pool({
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser');
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies from Brother Burton's code
+  extended: true
+}));
+
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
