1bd7ae0fa979367cc1aadbb9d7cc24d7370c04cc
Kayle Sillito <sil14003@byui.edu>
2017-07-10 16:43:03 -0600

    add conversation

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+let pg = require('pg');
+if (process.env.DATABASE_URL) {
+  pg.defaults.ssl = true;
+}
+
+let connString = process.env.DATABASE_URL || 'postgresql://ta:password@localhost:5432/messages';
+const { Pool } = require('pg');
+
+// console.log(connString);
+const pool = new Pool({
+  connectionString : connString
+});
+
+
 var express = require('express');
 var app = express();
 
@@ -11,11 +25,21 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(){
 	console.log("slashhhhh");
-	
+
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+//conversation.addConversation
+app.post('/addConversation', function addConversation(request, response) {
+	
+	var name = request.body.name;
+	var user = request.body.user;
+	console.log("added a conversation with the name " + name);
+	if(name){
+		pool.query('INSERT INTO conversation("name", "users") VALUES ($1, $2)', [name, user] );
+	}
+} );
 
